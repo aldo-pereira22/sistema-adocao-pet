821889d1b1a20109126feae482cc94556a1a30f1
Aldo pereira <aldopereira.gyn@gmail.com>
2022-05-02 20:52:31 -0300

    Validação dos dados do usuário

---
+++ back-end/controllers/UserController.js
@@ -0,0 +1,51 @@
+const User = require('../models/User')
+
+module.exports = class UserController {
+    static async register(req, res) {
+        const { name, email, phone, password, confirmpassword } = req.body
+
+        // Validações 
+        if (!name) {
+            res.status(422).json({ message: ' O nome é obrigatório' })
+            return
+
+        }
+        if (!email) {
+            res.status(422).json({ message: ' O email é obrigatório' })
+            return
+
+        }
+        if (!phone) {
+            res.status(422).json({ message: ' O telefone é obrigatório' })
+            return
+
+        }
+        if (!password) {
+            res.status(422).json({ message: ' A senha é obrigatória' })
+            return
+
+        }
+        if (!confirmpassword) {
+            res.status(422).json({ message: ' A confirmação de senha é obrigatória' })
+            return
+
+        }
+
+        if (password !== confirmpassword) {
+            res.status(422).json({ message: ' A senha e confirmação de senha devem ser iguais' })
+            return
+
+        }
+
+        // Verificar se o usuário existe
+        const userExist = await User.findOne({ email: email })
+        if (userExist) {
+            res.status(422).json({ message: "Este e-mail ja está cadastrado! Utilize outro email" })
+            return
+        }
+
+
+
+    }
+
+}
