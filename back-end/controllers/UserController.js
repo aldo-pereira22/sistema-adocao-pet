4341ae34e36c88ad64a15189d78c5b4d15c43282
Aldo pereira <aldopereira.gyn@gmail.com>
2022-05-02 23:12:07 -0300

    autenticação

--- back-end/controllers/UserController.js
+++ back-end/controllers/UserController.js
@@ -71,4 +71,39 @@ module.exports = class UserController {
 
     }
 
+    static async login(req, res) {
+        const { email, password } = req.body
+        if (!email) {
+            res.status(422).json({ message: ' O email é obrigatório' })
+            return
+        }
+        if (!password) {
+            res.status(422).json({ message: ' A senha é obrigatória' })
+            return
+
+        }
+
+
+        // Verifica se o usuário existe 
+        // let user = await User.find().or([{ email: email }])
+        let user = await User.findOne({ email: email })
+        console.log("LINHA 90", user)
+
+        if (!user) {
+            res.status(422).json({ message: "Não há usuário cadastrado com esse e-mail" })
+            return
+        }
+
+
+        // Verificar se a senha enviada é a correspondente no banco de dados    
+        const checkPassword = await bcrypt.compare(password, user.password)
+        if (!checkPassword) {
+            res.status(422).json({ message: "Senha inválida!" })
+            return
+
+        }
+
+        await createUserToken(user, req, res)
+    }
+
 }
