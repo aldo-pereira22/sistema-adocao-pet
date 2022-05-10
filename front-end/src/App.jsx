9c255c09ca3dca226ad0a75630242b8b436a2325
Aldo pereira <aldopereira.gyn@gmail.com>
2022-05-10 20:57:03 -0300

    Css global

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './components/pages/auth/Login';
+import Register from './components/pages/auth/Register';
+import Home from './components/pages/Home';
+
 function App() {
     return (
-    <div>
-        <h1> Get a pet</h1>
-    </div>
+        <Router >
+            <Routes>
+            <Route path="/" element={<Home/>} > </Route>
+            <Route path="/login" element={<Login />} > </Route>
+            <Route path="/register" element={<Register />} > </Route>
+
+            </Routes>
+        </Router>
     )
         
     }
