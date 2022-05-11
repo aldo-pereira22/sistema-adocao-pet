a3af172129cb2e9a4e8cd9c4f797f334bee4a869
Aldo pereira <aldopereira.gyn@gmail.com>
2022-05-11 11:16:59 -0300

    Componente de container

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,17 +8,20 @@ import Home from './components/pages/Home';
 // Components
 import Footer from './components/Layouts/Footer';
 import NavBar from './components/Layouts/NavBar';
+import Container from './components/Layouts/Container'
 
 function App() {
     return (
         <Router >
             <NavBar />
+            <Container>
             <Routes>
             <Route path="/" element={<Home/>} > </Route>
             <Route path="/login" element={<Login />} > </Route>
             <Route path="/register" element={<Register />} > </Route>
 
             </Routes>
+            </Container>
             <Footer />
         </Router>
     )
