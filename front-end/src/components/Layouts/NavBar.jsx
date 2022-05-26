06bf658cc482aba8b266b1c8003875bb9873f980
Aldo pereira <aldopereira.gyn@gmail.com>
2022-05-25 23:43:05 -0300

    Persistindo autenticação no front

--- front-end/src/components/Layouts/NavBar.jsx
+++ front-end/src/components/Layouts/NavBar.jsx
@@ -1,7 +1,14 @@
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo.png'
 import styles from './NavBar.module.css'
+import {useState, useContext} from 'react'
+
+
+// COntext authenticaded
+import { Context } from '../../context/UserContext' 
+
 function NavBar(){
+    const {authenticaded} = useContext(Context)
     return (
         <nav className={styles.navbar} >
             <div className={styles.navbar_logo}>
@@ -12,13 +19,22 @@ function NavBar(){
                 <li>
                     <Link to="/" >Adotar</Link>
                 </li>
+
+
+            
+            { authenticaded ? ( 
+                <>
+                <p>Logado</p>
+                </>
+            
+            ) : ( <>
                 <li>
                     <Link to="/login" >Entrar</Link>
                 </li>
                 <li>
                     <Link to="/register" >Cadastrar</Link>
                 </li>
-            
+            </>) }
             </ul>
         </nav>
     )
