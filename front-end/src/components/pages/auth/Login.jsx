a55dbe4ba1c5bf0ed8524f85c0e0110b077e703d
Aldo pereira <aldopereira.gyn@gmail.com>
2022-05-30 23:43:55 -0300

    Formulário de Login

--- front-end/src/components/pages/auth/Login.jsx
+++ front-end/src/components/pages/auth/Login.jsx
@@ -1,7 +1,50 @@
+import {useState, useContext} from 'react'
+import styles   from '../../form/Form.module.css'
+import { Link } from 'react-router-dom';
+
+
+// Context
+import {Context} from '../../../context/UserContext'
+import Input from '../../form/Input'
+
+
+
 function Login(){
+
+    function handleChange(e){
+
+    }
     return (
-        <section>
+        <section className={styles.form_container}>
             <h1>Login</h1>
+            <form action="">
+
+
+                <Input
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Digite seu e-mail"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Senha"
+                    type="password"
+                    name="password"
+                    placeholder="Digite sua senha"
+                    handleOnChange={handleChange}
+                />
+
+                <Input
+                    type="submit"
+                    value="Entrar"
+          
+                />
+
+            </form>
+            <p>
+                Não tem conta ? <Link to="/register" >Clique aqui</Link>
+            </p>
         </section>
     )
 }
