4d1b921923218988239880bb924ac0eae70dca3b
Aldo pereira <aldopereira.gyn@gmail.com>
2022-05-30 23:53:40 -0300

    Função de login

--- front-end/src/hooks/useAuth.js
+++ front-end/src/hooks/useAuth.js
@@ -43,6 +43,24 @@ export default function useAuth() {
         navigate('/')
     }
 
+    async function login(user) {
+        let msgText = "Login realizado com sucesso!"
+        let msgType = 'success'
+
+        try {
+
+            const data = await api.post('/users/login', user).then((response) => {
+                return response.data
+            })
+
+            await authUser(data)
+
+        } catch (error) {
+            msgText = error.response.data.message
+            msgType = 'error'
+        }
+    }
+
     function logout() {
         const msgText = 'Logout realizado com sucesso!'
         const msgType = 'success'
