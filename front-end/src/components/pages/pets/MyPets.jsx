306ffa84e63a88cba6401a91c6d43fc75d7896c2
Aldo pereira <aldopereira.gyn@gmail.com>
2022-06-07 23:09:05 -0300

    Página de pets

--- front-end/src/components/pages/pets/MyPets.jsx
+++ front-end/src/components/pages/pets/MyPets.jsx
@@ -1,7 +1,25 @@
+import {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+
 function MyPets(){
+    const [pets, setPets] = useState([])
+
     return (
+
         <section>
-            <h1>MyPets</h1>
+            <div>
+                <h1>My Pets</h1>
+                <Link to='/pet/add' > Cadastrar Pet</Link>
+            </div>
+            <div>
+                {pets.length > 0 && 
+                    <p>Meus pets Cadastrados</p>
+                }
+
+                {pets.length === 0 && <p>Não Há pets cadastrados </p> }
+
+
+            </div>
         </section>
     )
 }
