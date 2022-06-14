385b3ac94abdc22bf1acde80906d1e0a9c5d2e56
Aldo pereira <aldopereira.gyn@gmail.com>
2022-06-13 21:00:29 -0300

    Carregar pets cadastrados

--- front-end/src/components/pages/pets/MyPets.jsx
+++ front-end/src/components/pages/pets/MyPets.jsx
@@ -1,8 +1,12 @@
 import {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
-
+import RoundedImage from '../../Layouts/RoundedImage'
 function MyPets(){
     const [pets, setPets] = useState([])
+    const [token] = useState(localStorage.getItem('token') || '')
+
+    // hooks
+    import useFlashMessage from '../../../hooks/useFlashMessage'
 
     return (
 
@@ -15,7 +19,7 @@ function MyPets(){
                 {pets.length > 0 && 
                     <p>Meus pets Cadastrados</p>
                 }
-
+    RoundedImage
                 {pets.length === 0 && <p>Não Há pets cadastrados </p> }
 
 
