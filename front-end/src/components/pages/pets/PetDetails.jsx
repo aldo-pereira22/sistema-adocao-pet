db1a1733f464d6639cde8094932316614e7ab8b9
Aldo pereira <aldopereira.gyn@gmail.com>
2022-06-23 08:47:01 -0300

    Estruturação da págia de detalhes do pet

--- front-end/src/components/pages/pets/PetDetails.jsx
+++ front-end/src/components/pages/pets/PetDetails.jsx
@@ -18,9 +18,37 @@ function PetDetails(){
         })
     },[])
     return (
-        <h1>
-            {pet.name}
-        </h1>
+        <>
+            {pet.name && (
+                <section>
+                    <div>
+                        <h1>Conhecendo o Pet:{pet.name}</h1>
+                        <p>Se tiver interesse, marque uma visita para conhecê-lo</p>
+                    </div>
+
+                    <div>
+                        <p>{ pet.images.map((image, index) => (
+                            <img
+                                src={`${process.env.REACT_APP_API}/images/pets/${pet.image}`}
+                                alt={pet.name}
+                                key={index}                            
+                            />
+                        ))}</p>
+                    </div>
+                    <p>
+                        <span className="bold">Peso:</span> {pet.weight} kg
+                    </p>
+                    <p>
+                        <span className="bold">Idade:</span> {pet.age} anos
+                    </p>
+                    {token ? (
+                        <button>Solicitar uma visita</button>
+                    ) : (
+                        <p>Vc precisa <Link to="/register" > criar uma conta </Link> para adotara visita</p>
+                    ) }
+                </section>
+            )}
+        </>
     )
 }
 
